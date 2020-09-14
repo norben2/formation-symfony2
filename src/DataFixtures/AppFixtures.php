a3ff669e921ba769c403bf353bd01c5029fd2b6c
noreddine <noreddine@noreddine-ThinkPad-T430>
2020-09-14 13:43:01 +0200

    Installation of kaker package to have more fake results with fixtures, then installation of slugify package to create slugs for URLs

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,41 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use \App\Entity\Ad;
+use Cocur\Slugify\Slugify;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('FR-fr');
+        $slugify = new Slugify();
 
-        $manager->flush();
+
+        
+        $title = $faker -> sentence();
+        $slug = $slugify -> slugify($title);
+        $coverImage  = $faker -> imageUrl(1000, 350);
+        $introduction = $faker -> paragraph(2);
+        $content =  '<p>'.join('<p></p>',$faker ->paragraphs(5)).'</p>';
+
+        for($i =1; $i <= 30; $i++){
+            $ad = new Ad();
+            $ad -> setTitle($title)
+                -> setSlug($slug)
+                -> setPrice(\mt_rand(40, 200))
+                -> setIntroduction($introduction)
+                -> setContent($content)
+                -> setCoverImage($coverImage)
+                -> setRooms(\mt_rand(1,5));
+            
+             // $product = new Product();
+             // $manager->persist($product);
+             $manager->persist($ad);
+        } 
+         $manager->flush();
     }
 }
