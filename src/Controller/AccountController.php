e870f592e4405e38912c1a0592e2af7969b4bc66
noreddine <noreddine@noreddine-ThinkPad-T430>
2020-09-23 16:08:08 +0200

    added the way to disconnect

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccountController extends AbstractController
 {
     /**
+     * permet d'afficher et de gérer le formulaire de connexion
      * @Route("/login", name="account_login")
+     * @return Response
      */
     public function login()
     {
         return $this->render('account/login.html.twig', [
             'controller_name' => 'AccountController',
         ]);
+    }
+    
+    /**
+     * permet de se déconnecter
+     * @Route("/logout", name= "account_logout")
+     * @return void
+     */
+    public function logout(){
+        //rien
     }
 }
