f1f4e0ea1fe9605ccb59fef520de8b503d20a33c
noreddine <noreddine@noreddine-ThinkPad-T430>
2020-10-12 18:40:43 +0200

    added a controller to manage a user profile page to show his name, number of ads he has ... etc

---
+++ src/Controller/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user/{slug}", name="user_show")
+     * 
+     * on va utiliser le param converter de symfony pour avoir l'utilisateur 
+     * on argument de la fonction
+     */
+    public function index(User $user)
+    {
+        return $this->render('user/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
