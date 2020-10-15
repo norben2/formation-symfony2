ec6bc5b808d5ba44c9c22aab9006c5788e9c6390
noreddine <noreddine@noreddine-ThinkPad-T430>
2020-10-15 15:38:31 +0200

    added the role entity and created the tables

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -98,11 +98,17 @@ class User implements UserInterface
      */
     private $ads;
 
+    /**
+     * @ORM\ManyToMany(targetEntity=Role::class, mappedBy="users")
+     */
+    private $userRoles;
+
   
 
     public function __construct()
     {
         $this->ads = new ArrayCollection();
+        $this->userRoles = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -272,6 +278,34 @@ class User implements UserInterface
 
          public function eraseCredentials(){
              /* toto*/
+         }
+
+         /**
+          * @return Collection|Role[]
+          */
+         public function getUserRoles(): Collection
+         {
+             return $this->userRoles;
+         }
+
+         public function addUserRole(Role $userRole): self
+         {
+             if (!$this->userRoles->contains($userRole)) {
+                 $this->userRoles[] = $userRole;
+                 $userRole->addUser($this);
+             }
+
+             return $this;
+         }
+
+         public function removeUserRole(Role $userRole): self
+         {
+             if ($this->userRoles->contains($userRole)) {
+                 $this->userRoles->removeElement($userRole);
+                 $userRole->removeUser($this);
+             }
+
+             return $this;
          }
 }
 
